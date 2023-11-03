cf136bf4a86466fa3b0539b33598a07f92105da5
Grebenev Oleg Evgenevich (4095662) <OGrebenev@t1-consulting.ru>
2023-11-03 11:17:57 +0500

    Add Actions

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -18,14 +18,7 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a reponse for demo purposes.
     // Don't do this in real life :)
-
-    console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    console.log("Data fetch complete after 3 seconds.");
-
     return data.rows;
   } catch (error) {
     console.error("Database Error:", error);
@@ -166,7 +159,6 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
     return invoice[0];
   } catch (error) {
     console.error("Database Error:", error);
